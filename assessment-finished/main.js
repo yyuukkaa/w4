1935441a158a6d4a9c663fc219ebaf9e7f66a080
yyuukkaa <144975285+yyuukkaa@users.noreply.github.com>
2023-10-05 11:40:27 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -32,8 +32,8 @@ function result() {
   }
 
   if (document.getElementById("日本").checked) {
-    const weight = `${Math.round(110*0.454)} stone`;
-    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    const weight = `${Math.round(110*0.454)} キロ`;
+    const temperature =  `摂氏${Math.round((94-32) * 5 / 9)} 度`;
     newStory = newStory.replace('華氏40度', temperature);
     newStory = newStory.replace('110ポンド', weight);
   }
